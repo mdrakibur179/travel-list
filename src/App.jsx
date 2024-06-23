8674812723961c380be58e594b2634dfef7d0beb
mdrakibur179 <mdrakibur179@gmail.com>
2024-06-23 21:24:40 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import "./styles/index.css";
+
+function App() {
+  return (
+    <div>
+      <Logo />
+    </div>
+  );
+}
+
+function Logo() {
+  return <h1>🌴 Far Away 👜</h1>
+}
+
+function Form() {}
+
+function PackingList() {}
+
+function Stats() {}
+
+export default App;
