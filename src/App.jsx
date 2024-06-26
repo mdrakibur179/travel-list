21eca95a0622003ce5d5b17b16079d77817de975
mdrakibur179 <mdrakibur179@gmail.com>
2024-06-26 15:55:00 +0600

    added console log the entered data

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 /* eslint-disable react/prop-types */
+import { useState } from "react";
 import "./styles/index.css";
 
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
 ];
 
 function App() {
@@ -22,10 +23,45 @@ function Logo() {
 }
 
 function Form() {
+  const [quantity, setQuantity] = useState(1);
+  const [description, setDescription] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!description) {
+      alert("Enter the name of your desired item");
+      return;
+    }
+
+    const newItem = { id: Date.now(), description, quantity, packed: false };
+    console.log(newItem);
+
+    setQuantity(1);
+    setDescription("");
+  }
+
   return (
-    <div className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for 😍 your trip?</h3>
-    </div>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option key={num} value={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <button>Add</button>
+    </form>
   );
 }
 
@@ -44,7 +80,7 @@ function PackingList() {
 function Item({ itemObj }) {
   return (
     <li>
-      <span className={itemObj.packed ? "packed" : ""}>
+      <span style={itemObj.packed ? { textDecoration: "line-through" } : {}}>
         {itemObj.quantity} {itemObj.description}
       </span>
       <button>❌</button>
