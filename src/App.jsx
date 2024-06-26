998f241e6854ff13463490aebb12a4a61a5aa8e8
mdrakibur179 <mdrakibur179@gmail.com>
2024-06-26 15:03:17 +0600

    need help

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,10 @@
+/* eslint-disable react/prop-types */
 import "./styles/index.css";
+
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+];
 
 function App() {
   return (
@@ -24,7 +30,26 @@ function Form() {
 }
 
 function PackingList() {
-  return <div className="list">LIST</div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((i) => (
+          <Item key={i.id} itemObj={i} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item({ itemObj }) {
+  return (
+    <li>
+      <span className={itemObj.packed ? "packed" : ""}>
+        {itemObj.quantity} {itemObj.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
 }
 
 function Stats() {
