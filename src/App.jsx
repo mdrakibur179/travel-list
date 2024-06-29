e0f3c935c58ea6c7c1175217fdd4b1b7091f3a35
mdrakibur179 <mdrakibur179@gmail.com>
2024-06-29 13:53:51 +0600

    this is main branch with static components added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
 import "./styles/index.css";
 
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+];
+
 function App() {
   return (
     <div className="app">
